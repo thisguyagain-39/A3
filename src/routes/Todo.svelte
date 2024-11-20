e5644350099f6912b6fdbea5f863f8ae2c84b64e
thisguyagain-39 <cframpto@confederationcollege.ca>
2024-11-20 16:07:22 -0500

    added basic functions

---
+++ src/routes/Todo.svelte
@@ -0,0 +1,43 @@
+<script>
+
+let todoItem = $state('')
+
+let todoList = $state([])
+
+    function addItem() {
+
+        event.preventDefault()
+
+        if (todoItem == '') {
+
+            return;
+        }
+
+        todoList.push(todoItem)
+
+        todoItem = ''
+    }
+
+$inspect(todoList)
+
+</script>
+
+<form onsubmit={addItem}>
+
+    <input type="text" maxlength="120" bind:value={todoItem}>
+
+    <button type="submit"> Add </button>
+
+</form>
+
+<ul>
+
+    {#each todoList as item}
+    <li> {item}</li>
+    {/each}
+</ul>
+
+<style>
+
+
+</style>
